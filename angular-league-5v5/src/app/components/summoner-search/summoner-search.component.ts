a41f9d666106d085cfa442cafbbc55f2dcc3acf5
Leon Cugura <leoncugura@gmail.com>
2026-03-05 12:48:43 +0100

    find a player with his username and server tag

--- angular-league-5v5/src/app/components/summoner-search/summoner-search.component.ts
+++ angular-league-5v5/src/app/components/summoner-search/summoner-search.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
+import { PlayerService } from '../../services/player.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-summoner-search',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './summoner-search.component.html',
   styleUrl: './summoner-search.component.scss'
 })
 export class SummonerSearchComponent {
+  gameName: string = '';
+  tagLine: string = '';
+  isLoading: boolean = false;
+  errorMessage: string = '';
 
+  constructor(private playerService: PlayerService) {}
+
+  onSearch(): void {
+
+    console.log('Button clicked!'); 
+    console.log('gameName:', this.gameName);
+    console.log('tagLine:', this.tagLine);
+
+    if (!this.gameName || !this.tagLine) return;
+
+    this.isLoading = true;
+    this.errorMessage = '';
+
+    this.playerService.searchPlayer(this.gameName, this.tagLine).subscribe({
+      next: (player) => {
+        console.log('Player found:', player);
+        this.isLoading = false;
+      },
+      error: (err) => {
+        this.errorMessage = 'Failed to fetch player data. Please check the name and tag.';
+        this.isLoading = false;
+        console.log(err);
+      }
+    });
+  }
 }
